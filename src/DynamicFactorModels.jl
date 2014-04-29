e09027ab0e5bef2a03b4d118b3ce40c94bdb71eb
Johannes Degn <j@degn.de>
2014-04-29 11:02:47 +0200

    added hard thresholding to targeted predictors

--- src/DynamicFactorModels.jl
+++ src/DynamicFactorModels.jl
@@ -66,13 +66,25 @@ function targeted_predictors(y::Array{Float64,1}, w::Matrix{Float64}, x::Matrix{
     #lm(*("x1~", join(names(predictors[2:end]), "+")), predictors)
 
     design_matrix = hcat(w, x)
-    if thresholding == "hard"
+    if thresholding == "hard"  # TODO: hard thresholding does not work properly, only admits one variable plus the white covariance has negative diagonal elements
         coefficients = inv(design_matrix'*design_matrix)*(design_matrix'*y)  # OLS estimate
         residuals = design_matrix * coefficients
         # calcualte variance-covariance matrix according to White(1980) TODO: argument to choose if robust or not
         coefficient_covariance = inv(design_matrix'design_matrix)*(design_matrix'*diagm(residuals.^2)*design_matrix)*inv(design_matrix'design_matrix)
-        t_stat = coefficients./sqrt(diag(coefficient_covariance))
+        num_negative = sum(diag(coefficient_covariance) .< 0)
+        if num_negative > 0
+            for i in 1:size(coefficient_covariance)[1]
+                if coefficient_covariance[i, i] < 0
+                   coefficient_covariance[i, i] = -coefficient_covariance[i, i]
+               end
+            end
+            t_stat = coefficients./sqrt(diag(coefficient_covariance))
+            println("there were $num_negative negative coefficient variances which have been set positive. THIS IS UNACCEPTABLE!")
+        else
+            t_stat = coefficients./sqrt(diag(coefficient_covariance))
+        end
         critical_value = quantile(TDist(length(y)-length(coefficients)), 0.975)
+        println(sum(abs(t_stat[size(w)[2]+1:end]) .> critical_value))
         return abs(t_stat[size(w)[2]+1:end]) .> critical_value  # return significant t-stats associated with x variables only (Bai, Ng, 2008)
     end
     if thresholding == "soft"  # lasso coefficients which are non 0
